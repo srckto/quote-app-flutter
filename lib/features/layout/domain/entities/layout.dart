caaf690d9fb1873756f753e2b6e34c6a1841e85a
Ali Ismail <johndoe@example.com>
2022-09-26 11:42:18 +0300

    update

---
+++ lib/features/layout/domain/entities/layout.dart
@@ -0,0 +1,16 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class LayoutObject {
+  final String title;
+  final Icon icon;
+  final Widget screen;
+
+  LayoutObject({
+    required this.title,
+    required this.icon,
+    required this.screen,
+  });
+}
--- lib/features/layout/presentation/cubit/layout_cubit.dart
+++ lib/features/layout/presentation/cubit/layout_cubit.dart
@@ -2,31 +2,20 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:quote/core/utils/app_strings.dart';
+import 'package:quote/features/layout/domain/entities/layout.dart';
 import 'package:quote/features/quotes/presentation/screens/quotes_screen.dart';
 import 'package:quote/features/random_quote/presentation/screens/random_quote_screen.dart';
 
 part 'layout_state.dart';
 
-class LayoutItem {
-  final String title;
-  final Icon icon;
-  final Widget screen;
-
-  LayoutItem({
-    required this.title,
-    required this.icon,
-    required this.screen,
-  });
-}
-
 class LayoutCubit extends Cubit<LayoutState> {
-  final List<LayoutItem> items = [
-    LayoutItem(
+  final List<LayoutObject> items = [
+    LayoutObject(
       title: AppStrings.randomQuoteScreen,
       icon: Icon(Icons.touch_app),
       screen: RandomQuoteScreen(),
     ),
-    LayoutItem(
+    LayoutObject(
       title: AppStrings.quotesScreen,
       icon: Icon(Icons.touch_app_outlined),
       screen: QuotesScreen(),
